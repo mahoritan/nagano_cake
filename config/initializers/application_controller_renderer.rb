e2c513a297629bacfda9d0bf16440f6780ed7368
EC2 Default User <ec2-user@ip-172-31-32-239.ec2.internal>
2021-09-04 01:40:49 +0000

    [add]商品編集

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b3088e3ec23c4fe62010b164fa73597deaaa60ea4669cef58e0ebc52287b4e253e453d7b98e9a56c58261a2e4eedfaea42d5324f9fb5dd5164abe7eeb9524431'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  devise_for :admin
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :items, except:[:destroy]
+  end
 end
